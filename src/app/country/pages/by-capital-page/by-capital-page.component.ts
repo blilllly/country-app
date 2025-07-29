0f58ba5334dcf4cb5cef5d618d4d5c14c5b6c0d7
Billy Alvear <alvearbilly@gmail.com>
2025-07-28 22:39:56 -0500

    presentation and mapper

--- src/app/country/pages/by-capital-page/by-capital-page.component.ts
+++ src/app/country/pages/by-capital-page/by-capital-page.component.ts
@@ -1,6 +1,8 @@
-import { ChangeDetectionStrategy, Component } from '@angular/core';
+import { ChangeDetectionStrategy, Component, inject, signal } from '@angular/core';
 import { SearchInputComponent } from "../../components/search-input/search-input.component";
 import { CountryListComponent } from "../../components/country-list/country-list.component";
+import { CountryService } from '../../services/country.service';
+import { Country } from '../../interfaces/country.interface';
 
 @Component({
   selector: 'by-capital-page',
@@ -10,8 +12,23 @@ import { CountryListComponent } from "../../components/country-list/country-list
 })
 export class ByCapitalPageComponent {
 
-  onSearch(value: string) {
-    console.log(value);
+  countryService = inject(CountryService);
+
+  isLoading = signal(false);
+  isError = signal<string | null>(null);
+  countries = signal<Country[]>([]);
+
+  onSearch(query: string) {
+    if( this.isLoading() ) return;
+    this.isLoading.set(true);
+    this.isError.set(null);
+
+    this.countryService.searchByCapital(query)
+      .subscribe( (countries) => {
+        this.isLoading.set(false);
+        this.countries.set(countries);
+        console.log(countries);
+      })
   }
 
  }
